7830debc5ec9b3a9f5ed6d8a89b1a9c8ac7a65b8
quantum0059 <shivamsharma.anva@gmail.com>
2026-06-10 12:44:22 +0530

    refactor: update notification service interface

--- Drive/notification_app_be/src/services/interfaces/notification-service.interface.ts
+++ Drive/notification_app_be/src/services/interfaces/notification-service.interface.ts
@@ -1,6 +1,25 @@
-import { Notification } from "../../types/domain";
+import { Notification, NotificationType } from "../../types/domain";
+
+export interface NotificationListQuery {
+  page?: number;
+  pageSize?: number;
+  type?: NotificationType;
+}
+
+export interface NotificationFetchResult {
+  data: Notification | null;
+  error?: string;
+}
+
+export interface NotificationListResponse {
+  items: Notification[];
+  page: number;
+  pageSize: number;
+  total: number;
+  error?: string;
+}
 
 export interface NotificationService {
-  getNotification(id: string): Promise<Notification | null>;
-  listNotifications(): Promise<Notification[]>;
+  getNotification(id: string): Promise<NotificationFetchResult>;
+  listNotifications(query?: NotificationListQuery): Promise<NotificationListResponse>;
 }
